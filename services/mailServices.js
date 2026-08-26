b4693b73d45c61ade9083be923fbf539206b824f
himanshu_nishad <himanshunishad620@gmail.com>
2026-08-26 23:10:00 +0530

    done

--- services/mailServices.js
+++ services/mailServices.js
@@ -11,12 +11,11 @@ const transporter = nodemailer.createTransport({
 
 const sendEmail = async (to, subject, text) => {
   try {
-    const res = await transporter.sendMail({
+    await transporter.sendMail({
       to,
       subject,
       text,
     });
-    console.log(res);
   } catch (error) {
     console.log(error);
   }
