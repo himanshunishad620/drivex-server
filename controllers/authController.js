9ebc6c966945a9814a05f66b5f7d280a3f7174eb
himanshu_nishad <himanshunishad620@gmail.com>
2026-09-06 07:43:36 +0530

    origin

--- controllers/authController.js
+++ controllers/authController.js
@@ -44,6 +44,7 @@ exports.register = async (req, res) => {
 
 exports.login = async (req, res) => {
   const { email, password } = req.body;
+  console.log(email, password);
   const user = await User.findOne({ email });
   const isMatch = user?.password === password;
   if (!user || !isMatch)
